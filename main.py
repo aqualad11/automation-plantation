a6ac9fa90f9c4c1cf2611ecc2eab2a588dda442a
Jonathan <jonathanascencio.ja@gmail.com>
2020-05-04 20:36:57 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# variables
+
+# primitives
+w = 3
+x = 4 # int
+y = 'Manny' # string 
+hello = 2.4 # double
+
+
+# functions
+def plus(a, b):
+    s = a + b
+    return s
+
+t = plus(w, x)
+print(t)
+print('we are done now')
